b112d22d1c0a68f07ed65417b7b48f87f656f1a5
Ivan <ivanwhitemac@gmail.com>
2021-07-13 14:48:17 +0100

    Finished application

--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
-function helloWorld() {
-  alert("Hello World!")
+//Get the value from the string
+function getValue() {
+  document.getElementById("alert-success").classList.add("invisible")
+  document.getElementById("alert-fail").classList.add("invisible")
+
+  let userString = document.getElementById("userString").value
+
+  userString = userString.toLowerCase()
+
+  // Remove non alphanumeric characters and numbers
+  userString = userString.replace(/[^a-z]/gi, '')
+
+  let reversedString = reverseString(userString)
+
+  let isPalindrome = false
+
+  if (userString == reversedString) {
+    isPalindrome = true
+  }
+
+  displayString(reversedString, isPalindrome)
+}
+
+//Reverse the string
+function reverseString(userString) {
+  let revString = []
+
+  for (let i = userString.length - 1; i >= 0; i--) {
+    revString += userString[i]
+  }
+
+  return revString
+}
+
+//Display the message with the reversed string
+function displayString(revString, isPalindrome) {
+  if (isPalindrome == true) {
+    document.getElementById("msg-success").innerHTML = `${revString}`
+    document.getElementById("alert-success").classList.remove("invisible")
+  } else {
+    document.getElementById("msg-fail").innerHTML = `${revString}`
+    document.getElementById("alert-fail").classList.remove("invisible")
+  }
 }
